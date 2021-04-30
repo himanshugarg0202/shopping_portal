c44e311dd8e4f17ac0bbea3dd11d420f95980033
Himanshu Garg <himanshugarg02@gmail.com>
2021-04-30 15:23:35 +0530

    Updates

---
+++ precache-manifest.9026b0a0a478fa1594eb44f99af3d9f4.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "843b3a3334b795d7b83eae9be2ce104e",
+    "url": "/shopping_portal/index.html"
+  },
+  {
+    "revision": "8ad42161108f6f49e1c1",
+    "url": "/shopping_portal/static/css/main.c944e667.chunk.css"
+  },
+  {
+    "revision": "6cc8e176e36e59d77f1d",
+    "url": "/shopping_portal/static/js/2.24851a86.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/shopping_portal/static/js/2.24851a86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ad42161108f6f49e1c1",
+    "url": "/shopping_portal/static/js/main.05908b98.chunk.js"
+  },
+  {
+    "revision": "c1a5f0d91aff0c737a74",
+    "url": "/shopping_portal/static/js/runtime-main.d44e8ad5.js"
+  },
+  {
+    "revision": "fee85752ea2fc94423d5a5e2a7ee45ae",
+    "url": "/shopping_portal/static/media/Boy.fee85752.jpg"
+  },
+  {
+    "revision": "74cc799092af3e216c36088b76936f60",
+    "url": "/shopping_portal/static/media/Girl.74cc7990.jpg"
+  },
+  {
+    "revision": "6a4bc03fde14af57596585f91807bb47",
+    "url": "/shopping_portal/static/media/background.6a4bc03f.jpg"
+  },
+  {
+    "revision": "769b55e105e3dc6af4dec290d8f511c4",
+    "url": "/shopping_portal/static/media/dress.769b55e1.jpg"
+  },
+  {
+    "revision": "355777353444960a78639cbb8e72ad94",
+    "url": "/shopping_portal/static/media/frock.35577735.jpg"
+  },
+  {
+    "revision": "6d77f247ba783b5e844734ccc61184f4",
+    "url": "/shopping_portal/static/media/gown.6d77f247.jpg"
+  },
+  {
+    "revision": "87e5b72a1509d652b50ed832608d9e9c",
+    "url": "/shopping_portal/static/media/midi.87e5b72a.jpg"
+  },
+  {
+    "revision": "023ad8463fb87a126a252d53fcc02ba7",
+    "url": "/shopping_portal/static/media/short.023ad846.jpg"
+  },
+  {
+    "revision": "4b4b3b6def2e63666de6bf5fe8988769",
+    "url": "/shopping_portal/static/media/suit.4b4b3b6d.jpg"
+  }
+]);
